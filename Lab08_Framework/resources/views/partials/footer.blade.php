0ab8ea8f20680dd89ad0d2df9af4f6136751aee2
Khang <anhkhang784@gmail.com>
2025-10-06 20:45:21 +0700

    Push Bai 01 + Bai 02 + Bai 03

---
+++ Lab08_Framework/resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+<hr>
+<small>&copy; HUIT – Khoa CNTT. Laravel 12 Lab.</small>
+</footer>
---
+++ Lab08_Framework/resources/views/partials/nav.blade.php
@@ -0,0 +1,6 @@
+<nav style="padding:12px;background:#111827;color:white">
+
+<a href="{{ url('/') }}" style="color:#fff">Trang chủ</a>
+<a href="{{ route('products.index') }}" style="color:#fff">Products</a>
+<a href="" style="color:#fff">Tạo</a>
+</nav>
